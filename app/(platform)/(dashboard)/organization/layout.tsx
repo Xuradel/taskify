438aacde52953d2c0bc308218cead118954339f7
Xuradel <leagueofxuradel@gmail.com>
2023-11-19 18:58:21 -0600

    added server actions

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "../_components/sidebar";
+
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main
@@ -6,7 +8,7 @@ const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
     >
       <div className="flex gap-x-7">
         <div className="w-64 shrink-0 hidden md:block">
-            
+            <Sidebar />
         </div>
         {children}
       </div>

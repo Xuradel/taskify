3f002c667e58fb7afd4e415bfa69e949be41d9b3
Xuradel <leagueofxuradel@gmail.com>
2023-11-20 19:53:57 -0600

    added reusable form

--- app/(platform)/(dashboard)/organization/[organizationId]/form-delete.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form-delete.tsx
@@ -1,11 +1,13 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { useFormStatus } from "react-dom"
+import { Button } from "@/components/ui/button";
+import { useFormStatus } from "react-dom";
 
 export const FormDelete = () => {
-    const { pending } = useFormStatus()
-    return(
-        <Button disabled={pending} type="submit" variant="destructive" size="sm">Delete</Button>
-    )
-}
+  const { pending } = useFormStatus();
+  return (
+    <Button disabled={pending} type="submit" variant="destructive" size="sm">
+      Delete
+    </Button>
+  );
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,20 +1,36 @@
 "use client";
 
-import { create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
-import { useFormState } from "react-dom";
-import { FormInput } from "./form-input";
-import { FormButton } from "./form-button";
-
+import { useAction } from "@/hooks/use-action";
+import { createBoard } from "@/actions/create-board";
+import { FormInput } from "@/components/form/form-input";
+import { FormSubmit } from "@/components/form/form-submit";
 export const Form = () => {
-    const initialState = {message: null, errors: {}};
-    const [state, dispatch] = useFormState(create, initialState);
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data, "SUCESS");
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
   return (
-    <form action={dispatch}>
-        <div className="flex flex-col space-y-2">
-        <FormInput errors={state?.errors}/>
+    <form action={onSubmit}>
+      <div className="flex flex-col space-y-2">
+        <FormInput 
+        label="Board Title"
+        errors={fieldErrors} 
+        id="title"
+        />
       </div>
-      <FormButton />
+      <FormSubmit>
+        Save
+      </FormSubmit>
     </form>
   );
 };

3fecbac6c63c0e4be462ec83dc4c144f641b0814
Xuradel <leagueofxuradel@gmail.com>
2023-11-24 21:21:12 -0600

    added delete and update boards

---
+++ actions/update-board/types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod"
+
+import { Board } from "@prisma/client"
+
+import { ActionState } from "@/lib/create-safe-action"
+
+import { UpdatedBoard } from "./schema"
+
+export type InputType = z.infer<typeof UpdatedBoard>;
+export type Returntype = ActionState<InputType, Board>;
---
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,9 @@
+const BoardIdPage = () =>{
+    return(
+        <div>
+            board id page
+        </div>
+    )
+}
+
+export default BoardIdPage
